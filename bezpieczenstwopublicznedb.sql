1302e0bb7d4a43d1a9ab435be971e5f76e79a213
Holmings <patrycjax@student.agh.edu.pl>
2023-06-22 22:02:23 +0200

    Update bezpieczenstwopublicznedb.sql

--- bezpieczenstwopublicznedb.sql
+++ bezpieczenstwopublicznedb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 10 Cze 2023, 16:24
--- Wersja serwera: 10.4.27-MariaDB
--- Wersja PHP: 8.2.0
+-- Generation Time: Cze 22, 2023 at 09:56 PM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `bezpieczenstwopublicznedb`
+-- Database: `bezpieczenstwopublicznedb`
 --
 
 -- --------------------------------------------------------
@@ -28,18 +28,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cechyzdarzenia` (
-  `zgloszenie_id` int(10) NOT NULL,
   `opis_sprawcy` text CHARACTER SET utf32 COLLATE utf32_polish_ci DEFAULT NULL,
   `opis_zdarzenia` longtext CHARACTER SET utf32 COLLATE utf32_polish_ci NOT NULL,
-  `liczba_sprawcow` int(3) DEFAULT NULL
+  `liczba_sprawcow` int(3) DEFAULT NULL,
+  `zgloszenie_id` int(10) NOT NULL,
+  `miejsce_zdarzenia` varchar(100) NOT NULL,
+  `godzina_zdarzenia` time NOT NULL,
+  `data_zdarzenia` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `cechyzdarzenia`
+-- Dumping data for table `cechyzdarzenia`
 --
 
-INSERT INTO `cechyzdarzenia` (`zgloszenie_id`, `opis_sprawcy`, `opis_zdarzenia`, `liczba_sprawcow`) VALUES
-(1, 'Przystojny brunet o niebieskich oczach. Wysoki około 180 cm. Dosyć szczupły. Blizna na policzku. Ubrany na czarno.', 'Wszystko działo się gdy wracałam ze szkoły. Mężczyzna podbiegł i wyrwał mi torebkę po czym zaczął uciekać i zniknął na rogu ulic Czarnowiejska i Piastowska.', 1);
+INSERT INTO `cechyzdarzenia` (`opis_sprawcy`, `opis_zdarzenia`, `liczba_sprawcow`, `zgloszenie_id`, `miejsce_zdarzenia`, `godzina_zdarzenia`, `data_zdarzenia`) VALUES
+('lol', 'lol', 1, 3, 'lol', '12:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -56,6 +59,13 @@ CREATE TABLE `sprawcy` (
   `opis` mediumtext CHARACTER SET utf32 COLLATE utf32_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `sprawcy`
+--
+
+INSERT INTO `sprawcy` (`sprawca_id`, `zgloszenie_id`, `imie`, `nazwisko`, `data_urodzenia`, `opis`) VALUES
+(1, 3, '', '', NULL, 'lol');
+
 -- --------------------------------------------------------
 
 --
@@ -67,15 +77,22 @@ CREATE TABLE `użytkownicy` (
   `login` varchar(50) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   `hasło` varchar(255) NOT NULL,
   `rola` varchar(20) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
-  `email` varchar(100) NOT NULL
+  `email` varchar(100) NOT NULL,
+  `imię` varchar(50) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` varchar(50) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `użytkownicy`
+-- Dumping data for table `użytkownicy`
 --
 
-INSERT INTO `użytkownicy` (`user_id`, `login`, `hasło`, `rola`, `email`) VALUES
-(1, 'lol', 'lol123', 'cywil', 'lol@poczta.pl');
+INSERT INTO `użytkownicy` (`user_id`, `login`, `hasło`, `rola`, `email`, `imię`, `nazwisko`) VALUES
+(1, '', '', '', '', '', ''),
+(9, '', '', '', '', '', ''),
+(10, '', '', '', '', '', ''),
+(11, '', '', '', '', '', ''),
+(12, '', '', '', '', '', ''),
+(13, '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -84,20 +101,24 @@ INSERT INTO `użytkownicy` (`user_id`, `login`, `hasło`, `rola`, `email`) VALUE
 --
 
 CREATE TABLE `zgłoszenia` (
-  `zgloszenie_id` int(10) NOT NULL,
-  `user_id` smallint(6) DEFAULT NULL,
   `tytuł` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,
+  `zgloszenie_id` int(10) NOT NULL,
+  `user_id` smallint(6) NOT NULL,
   `godzina_zgloszenia` time NOT NULL,
   `data_zgloszenia` date NOT NULL,
   `status` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `zgłoszenia`
+-- Dumping data for table `zgłoszenia`
 --
 
-INSERT INTO `zgłoszenia` (`zgloszenie_id`, `user_id`, `tytuł`, `godzina_zgloszenia`, `data_zgloszenia`, `status`) VALUES
-(1, 1, 'kradzież', '17:33:30', '2023-06-01', 'przyjęte');
+INSERT INTO `zgłoszenia` (`tytuł`, `zgloszenie_id`, `user_id`, `godzina_zgloszenia`, `data_zgloszenia`, `status`) VALUES
+('', 1, 9, '21:50:52', '2023-06-22', 'przyjęte'),
+('', 2, 10, '21:51:55', '2023-06-22', 'przyjęte'),
+('', 3, 11, '21:52:27', '2023-06-22', 'przyjęte'),
+('', 4, 12, '21:53:46', '2023-06-22', 'przyjęte'),
+('', 5, 13, '21:54:17', '2023-06-22', 'przyjęte');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -127,45 +148,51 @@ ALTER TABLE `użytkownicy`
 --
 ALTER TABLE `zgłoszenia`
   ADD PRIMARY KEY (`zgloszenie_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `zgłoszenia_ibfk_3` (`user_id`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `sprawcy`
+-- AUTO_INCREMENT for table `sprawcy`
 --
 ALTER TABLE `sprawcy`
-  MODIFY `sprawca_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `sprawca_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT dla tabeli `zgłoszenia`
+-- AUTO_INCREMENT for table `użytkownicy`
+--
+ALTER TABLE `użytkownicy`
+  MODIFY `user_id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `zgłoszenia`
 --
 ALTER TABLE `zgłoszenia`
-  MODIFY `zgloszenie_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `zgloszenie_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Ograniczenia dla zrzutów tabel
+-- Constraints for dumped tables
 --
 
 --
--- Ograniczenia dla tabeli `cechyzdarzenia`
+-- Constraints for table `cechyzdarzenia`
 --
 ALTER TABLE `cechyzdarzenia`
-  ADD CONSTRAINT `cechyzdarzenia_ibfk_1` FOREIGN KEY (`zgloszenie_id`) REFERENCES `zgłoszenia` (`zgloszenie_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_zgloszenie` FOREIGN KEY (`zgloszenie_id`) REFERENCES `zgłoszenia` (`zgloszenie_id`);
 
 --
--- Ograniczenia dla tabeli `sprawcy`
+-- Constraints for table `sprawcy`
 --
 ALTER TABLE `sprawcy`
   ADD CONSTRAINT `sprawcy_ibfk_1` FOREIGN KEY (`zgloszenie_id`) REFERENCES `zgłoszenia` (`zgloszenie_id`);
 
 --
--- Ograniczenia dla tabeli `zgłoszenia`
+-- Constraints for table `zgłoszenia`
 --
 ALTER TABLE `zgłoszenia`
-  ADD CONSTRAINT `zgłoszenia_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `użytkownicy` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT `zgłoszenia_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `użytkownicy` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
