0c80b10085b1caf62e54244ce88eb60eb0ce2e0a
Holmings <patrycjax@student.agh.edu.pl>
2023-06-22 22:02:15 +0200

    it is what it is

--- app.py
+++ app.py
@@ -37,6 +37,19 @@ def zgloszenie():
             insert_query = "INSERT INTO zgłoszenia (tytuł, godzina_zgloszenia, data_zgloszenia, status) " \
                         "VALUES ('', %s, %s, 'przyjęte')"
             insert_values = (godzina_zgloszenia, data_zgloszenia)
+
+            insert_query = "INSERT INTO użytkownicy (login, hasło, rola, email, imię, nazwisko) " \
+                        "VALUES ('', '', '','','','')"
+            insert_values = ()
+            cursor.execute(insert_query, insert_values)
+            cnx.commit()            
+            
+            cursor.execute("SELECT LAST_INSERT_ID()")
+            user_id = cursor.fetchone()[0]
+
+            insert_query = "INSERT INTO zgłoszenia (user_id, tytuł, godzina_zgloszenia, data_zgloszenia, status) VALUES (%s,'', %s, %s, 'przyjęte')" ##tu trzeba dać jeszcze user_id który powinien gdzieś być przechowywany przy zalogowaniu 
+            insert_values = (user_id, godzina_zgloszenia, data_zgloszenia)
+
             cursor.execute(insert_query, insert_values)
             cnx.commit()
 
@@ -44,11 +57,14 @@ def zgloszenie():
             zgloszenie_id = cursor.lastrowid
 
             # Wstawienie danych do tabeli cechyzdarzenia z wykorzystaniem pobranego zgloszenie_id
-            insert_query = "INSERT INTO cechyzdarzenia (zgloszenie_id, opis_sprawcy, opis_zdarzenia, liczba_sprawcow, miejsce, godzina) " \
-                        "VALUES (%s, %s, %s, %s, %s, %s)"
+            insert_query = "INSERT INTO cechyzdarzenia (zgloszenie_id, opis_sprawcy, opis_zdarzenia, liczba_sprawcow, miejsce_zdarzenia, godzina_zdarzenia, data_zdarzenia) " \
+                        "VALUES (%s, %s, %s, %s, %s, %s,NULL)"
             insert_values = (zgloszenie_id, opis_sprawcy, opis, liczba_sprawcow, miejsce, godzina)
             cursor.execute(insert_query, insert_values)
             cnx.commit()
+
+            #wstawianie danych do tabeli użytkownicy
+
 
             # Wstawienie danych do tabeli sprawcy
             insert_query = "INSERT INTO sprawcy (zgloszenie_id, imie, nazwisko, data_urodzenia, opis) " \
