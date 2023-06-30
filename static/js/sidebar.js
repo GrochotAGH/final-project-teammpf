417b8692314096a8c6cc512115db1a068d748f2c
Filip Kuś <filipkarolkus@gmail.com>
2023-06-30 19:44:57 +0200

    Frontend 6.0
    - Dodanie dynamicznych sidebarów,
    - Wyrównanie ikon z tytułami w sekcjach formularza głównego,
    - Poprawa wyglądów strony wyszukiwania dla policjanta, zgłoszeń prywatnego użytkownika i wyświetlania zgłoszeń,
    - Poprawa i ujednolicenie wyglądu pasków nawigacji na wszystkich stronach,
    - Naprawa drobnych błędów związanych z przekierowaniem.
    -

---
+++ static/js/sidebar.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "320px";
+  document.getElementById("main").style.marginRight = "320px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+}
